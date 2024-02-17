eaaf42367b1ab62e56a1e67f110a50aa3d182e4b
Anuj kumar <73796112+Anujkumar6316@users.noreply.github.com>
2024-02-17 19:30:14 +0000

    added the uesrcreationform

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.login,name='login'),
+    path('register',views.register_user,name='register_user'),
 ]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .forms import Register_user
 
 # Create your views here.
 def login(request):
     return render(request,'users/login.html')
+
+def register_user(request):
+    if request.method == "POST":
+        form = Register_user(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,'users/login.html')
+    else:
+        form = Register_user()
+    return render(request,'users/register_user.html',{'form':form})
